b666c1939a39b83ff834f60345059f15ddfa84b8
Doriane Papilloud <doriane.papillou@hes-so.ch>
2022-03-05 12:26:58 +0100

    Login 3.0

--- app/src/main/java/com/example/dogbreedingdoga/LoginTabFragment.java
+++ app/src/main/java/com/example/dogbreedingdoga/LoginTabFragment.java
@@ -8,12 +8,32 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.example.dogbreedingdoga.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class LoginTabFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.login_tab_fragment, container, false);
+
+
+//        email = root.findViewById(R.id.tab_layout);
+//        viewPager = root.findViewById(R.id.view_pager);
+//
+//        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Create new account"));
+//        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+//
+//
+//        final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(), this,tabLayout.getTabCount());
+//        viewPager.setAdapter(adapter);
+//        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+//
+//        tabLayout.setTranslationY(300);
+//        tabLayout.setAlpha(v);
+//        tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(100).start();
+
+
 
         return root;
     }
