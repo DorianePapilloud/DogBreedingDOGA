5d2d75b0bfb1e89c60ba703676427557a18bc064
Gaétan Mottet <gaetan.mottet@students.hevs.ch>
2022-03-24 11:17:48 +0100

    Create Account + Login ok. Need correct target :)

--- app/src/main/java/com/example/dogbreedingdoga/ui/MainActivity.java
+++ app/src/main/java/com/example/dogbreedingdoga/ui/MainActivity.java
@@ -1,33 +1,57 @@
 package com.example.dogbreedingdoga.ui;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.dogbreedingdoga.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_welcome);
+//        Intent intent = new Intent(this, WelcomeActivity.class);
+//        startActivity(intent);
+//        }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
-        Intent intent = new Intent(this, WelcomeActivity.class);
-        startActivity(intent);
-        }
+        getLayoutInflater().inflate(R.layout.activity_main, frameLayout);
+
+        setTitle(getString(R.string.app_name));
+//        navigationView.setCheckedItem(R.id.nav_none);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setTitle(getString(R.string.app_name));
+//        navigationView.setCheckedItem(R.id.nav_none);
+    }
 
-
-//public class MainActivity extends AppCompatActivity {
-//
 //    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.login_tab_fragment);
-//
+//    public void onBackPressed() {
+//        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+//            drawerLayout.closeDrawer(GravityCompat.START);
+//            return;
+//        }
+//        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+//        alertDialog.setTitle(getString(R.string.action_logout));
+//        alertDialog.setCancelable(false);
+//        alertDialog.setMessage(getString(R.string.logout_msg));
+//        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.action_logout), (dialog, which) -> logout());
+//        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.action_cancel), (dialog, which) -> alertDialog.dismiss());
+//        alertDialog.show();
 //    }
-//}
+
+}
+
+
 
