6fe75bd88d0e0033b0b4935641a703c6f2bee558
Doriane Papilloud <doriane.papillou@hes-so.ch>
2022-03-25 16:15:34 +0100

    View add a dog finished

--- app/src/main/java/com/example/dogbreedingdoga/ui/MainActivity.java
+++ app/src/main/java/com/example/dogbreedingdoga/ui/MainActivity.java
@@ -1,16 +1,79 @@
 package com.example.dogbreedingdoga.ui;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.dogbreedingdoga.R;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    //code for implementing camera
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //code for implementing camera
+        imageView = findViewById(R.id.iv_add_picture_dog);
+        imageView.setOnClickListener(view -> mGetContent.launch("image/*"));
+        //end
+
+
+        //this code will open a calendar in which you can select a date
+        ImageButton btnDate = (ImageButton) findViewById(R.id.ib_birth);
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "Date picker");
+            }
+        });
+
     }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(currentDateString);
+    }
+
+    ActivityResultLauncher<String> mGetContent = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri result) {
+                    if(result != null){
+                        imageView.setImageURI(result);
+                    }
+                }
+            }
+    );
+
+
 }
